1685b72687f174c32d78b89be5d61b768bc3dc73
muhammad kamal <muhammedkamal022@gmail.com>
2021-06-22 16:14:32 +0200

    create repo from json factory

--- lib/data/repository_model.dart
+++ lib/data/repository_model.dart
@@ -8,4 +8,13 @@ class Repository {
     required this.userAvatarUrl,
     required this.userName,
   });
+  factory Repository.fromJson(Map<String, dynamic> json) {
+    return Repository(
+      description: json['description'],
+      name: json['jd_scripts'],
+      userAvatarUrl: json['owner']['avatar_url'],
+      userName: json['owner']['login'],
+      noOfStarts: json['stargazers_count'],
+    );
+  }
 }
