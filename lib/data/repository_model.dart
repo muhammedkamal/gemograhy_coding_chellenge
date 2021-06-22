efad1af1c18df5fa908383c591022ab2605d10d7
muhammad kamal <muhammedkamal022@gmail.com>
2021-06-22 15:36:11 +0200

    create repo model

---
+++ lib/data/repository_model.dart
@@ -0,0 +1,11 @@
+class Repository {
+  String name, description, userName, userAvatarUrl;
+  int noOfStarts;
+  Repository({
+    required this.description,
+    required this.name,
+    required this.noOfStarts,
+    required this.userAvatarUrl,
+    required this.userName,
+  });
+}
