471e4ae7601bf8970e4554054b43063bb5f23fb6
muhammad kamal <muhammedkamal022@gmail.com>
2021-06-22 15:15:03 +0200

    initial point for the chellange

---
+++ android/app/src/main/kotlin/resume/mukamal/ninja/gemo_coding_ch/MainActivity.kt
@@ -0,0 +1,6 @@
+package resume.mukamal.ninja.gemo_coding_ch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Github Most Stared',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
